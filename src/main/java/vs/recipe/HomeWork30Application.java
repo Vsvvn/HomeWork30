546e0d9be1e9c1322643e0f2daf2b6b94b2a5203
Vsvvn <Vladsun@yandex.ru>
2023-02-01 09:46:45 +0300

    Initial commit

---
+++ src/main/java/vs/recipe/HomeWork30Application.java
@@ -0,0 +1,13 @@
+package vs.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork30Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork30Application.class, args);
+	}
+
+}
---
+++ src/test/java/vs/recipe/HomeWork30ApplicationTests.java
@@ -0,0 +1,13 @@
+package vs.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork30ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
