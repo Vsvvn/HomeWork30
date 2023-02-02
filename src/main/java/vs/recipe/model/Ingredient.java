a95c6c2bb2124e255d2161447b48fab83466936a
Vsvvn <Vladsun@yandex.ru>
2023-02-02 17:42:37 +0300

    Доработано.

--- src/main/java/vs/recipe/model/Ingredient.java
+++ src/main/java/vs/recipe/model/Ingredient.java
@@ -11,4 +11,9 @@ public class Ingredient {
     private String name;
     private int count;
     private String measureUnit;
+
+    @Override
+    public String toString() {
+        return name + " - " + count + " " + measureUnit;
+    }
 }
--- src/main/java/vs/recipe/model/Recipe.java
+++ src/main/java/vs/recipe/model/Recipe.java
@@ -13,4 +13,21 @@ public class Recipe {
     private int cookingTime;
     private List<Ingredient> ingredients;
     private List<String> steps;
+
+        @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(name).append("\n")
+                .append("Время приготовления ").append(cookingTime).append(" минут").append("\n")
+                .append("Ингредеинты: ").append("\n");
+        for (Ingredient ingredient : ingredients) {
+            stringBuilder.append("• ").append(ingredient).append("\n");
+        }
+        stringBuilder.append("Инструкция приготовления: ").append("\n");
+        int counter = 1;
+        for (String step : steps) {
+            stringBuilder.append(counter++).append(". ").append(step).append("\n");
+        }
+        return stringBuilder.toString();
+    }
 }
